7ccd13b900dd00579a9b5b3d73781486943dc5f3
鲁子银 <ziyin.lu@dmall.com>
2019-04-28 13:53:05 +0800

    add gif

--- mul_tmux.sh
+++ mul_tmux.sh
@@ -15,10 +15,11 @@ done
 
 argAll=`echo $* | awk '{print $NF}'`
 
-if [[ ! "$identify"="" ]] ; then
+if [[ "$identify"="" ]] ; then
 	SSHCMD="ssh -o StrictHostKeyChecking=no -l $user -p $port "
 else
 	SSHCMD="ssh -o StrictHostKeyChecking=no  -i $identify -l $user -p $port "
+fi
 
 j=0
 for i in `cat $argAll`; do
